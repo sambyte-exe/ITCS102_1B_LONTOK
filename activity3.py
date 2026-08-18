028afcc307cbbcfa1517212ad589969547cc5b12
sambyte-exe <samkylit12@gmail.com>
2026-08-18 20:13:17 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,34 @@
+#using variables
+
+#int
+age = 18
+book = 49
+pets = 14
+silbings = 5
+
+#float
+budget = 600.45
+weigth = 46.9
+pressure = 103.78
+heartrate = 72.5
+
+#string
+surname = "lontok"
+favcolor = "pink"
+favbook = "when the morning comes"
+last_school_attended = "quezon national high school"
+
+#boolean
+isPretty = True
+isSmart = True
+isRich = False
+isHonest = True
+
+print("My age is ", age)
+
+print("My budget is ", budget)
+
+print("My favorite color is ", favcolor)
+
+print("I am pretty! Which is ", isPretty)
+
