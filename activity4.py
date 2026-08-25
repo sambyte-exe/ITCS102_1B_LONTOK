6382f7115384cd383bc6d85cffc04ea8679c5182
Samkiii <samkylit12@gmail.com>
2026-08-26 00:03:40 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,12 @@
+#escape sequence
+
+#\n
+#\t
+#\r
+#\b
+#\\
+#\"
+
+phrase = "\\Bloom where \r\"you want to be, \n\t\t\tthe sunlight will \b\b\b\bnaturally follows you.\" \n\t\tChocolate is like \\happiness, \n\tit can be sweet or bitter."
+
+print(phrase)
