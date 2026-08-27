35c97834d467ea89c0bf1eb6133aabb58a431824
Samkiii <samkylit12@gmail.com>
2026-08-27 21:11:57 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,24 @@
+#operator symbols
+
+chopper = 12
+leon = 6
+
+sum = chopper + leon
+difference = chopper - leon
+product = chopper * leon
+qoutient = chopper / leon
+modulus = chopper % leon
+exponential = chopper ** leon
+floordivision = chopper // leon
+
+print("The sum of", chopper, "and", "is", sum)
+print("The difference of", chopper, "and", leon, "is", difference)
+print("The product of", chopper, "and", leon, "is", product)
+print("The qoutient of", chopper, "and", leon, "is", qoutient)
+print("The modulus of", chopper, "and", leon, "is", modulus)
+print("The exponential of", chopper, "and", leon, "is", exponential)
+print("The floor divison of", chopper, "and", leon, "is", floordivision)
+
+
+
+
