fd4c1d1ead593211b045d2a1b6c84bf0fc014429
Samkiii <samkylit12@gmail.com>
2026-09-01 10:56:00 +0800

    Add files via upload

---
+++ code_challenge1.py
@@ -0,0 +1,24 @@
+import shutil
+
+width = shutil.get_terminal_size().columns
+
+print("*".center(width))
+print("*     *".center(width))
+print("*           *".center(width))
+print("*                 *".center(width))
+print("*                       *".center(width))
+print("**                              **".center(width))
+print("**                              **".center(width))
+print("**                              **".center(width))
+print("**        Hi, My Name is        **".center(width))
+print("**   Samantha Nicole U. Lontok  **".center(width))
+print("**         From BSIT-1B         **".center(width))
+print("**                              **".center(width))
+print("**                              **".center(width))
+print("**                              **".center(width))
+print("**                              **".center(width))
+print("**                              **".center(width))
+print("**                              **".center(width))
+print("**                              **".center(width))
+print("** *  *  *  *  *  *  *  *  *  * **".center(width))
+print("** *  *  *  *  *  *  *  *  *  * **".center(width))
