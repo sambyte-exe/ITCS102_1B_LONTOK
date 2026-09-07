b79cb9275218b9600d5f8676c5ec4b0319d66254
Samkiii <samkylit12@gmail.com>
2026-09-07 11:15:31 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,13 @@
+#basic if else program
+
+Username = "sambyte07"
+Password = "Miracle_Domain24"
+
+s = input("Input USERNAME ----> ")
+u = input("Input PASSWORD  ----> ")
+
+if s == Username and u == Password:
+           print("Username and Password Correct")
+
+else: 
+           print("Incorrect Username")
