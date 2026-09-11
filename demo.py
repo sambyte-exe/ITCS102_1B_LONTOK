e472dd86e3bfc4781ac25ef8d57bab48b61ba081
Samkiii <samkylit12@gmail.com>
2026-09-11 10:55:54 +0800

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,11 @@
+#the use of boolean 
+#True or False
+#Single detector
+
+is_beautiful = bool(input("Are you beautiful enough, inside and out?"))
+
+if is_beautiful == True:
+                           print("You're beautiful, just outside. So, focus in your inside.")
+
+else:
+                           print("You're beautiful, inside and out!")
